e1c04cb0a21ae6039500271d852d0a8697cb9172
RayaLitova <raya.litova23@gmail.com>
2026-04-16 20:58:33 +0300

    Added week 8 problems

---
+++ Week8/problems.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE EmptyDataDeriving #-}
+-- cover all cases!
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
+-- warn about incomplete patterns v2
+{-# OPTIONS_GHC -fwarn-incomplete-uni-patterns #-}
+-- write all your toplevel signatures!
+{-# OPTIONS_GHC -fwarn-missing-signatures #-}
+-- use different names!
+{-# OPTIONS_GHC -fwarn-name-shadowing #-}
+-- use all your pattern matches!
+{-# OPTIONS_GHC -fwarn-unused-matches #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+
+-- Да се дефинира тип за двоично дърво
+-- Да се напише функция, която намира височината на дървото
+-- Да се напише функция, която намира броя на всички елементи на дървото
+-- Да се напише функция, която проверява дали дадена стойност се съдържа в дървото
+-- Да се напише фунцкия, която приема дърво и връща списък от всички пътища от корена до листо
+
+-- Да се дефинира тип за дърво (не двоично)
+-- Да се реализират горните 4 функции за него
