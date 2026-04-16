709001c4070407331cfb4fa996c21b9d95e50d58
RayaLitova <raya.litova23@gmail.com>
2026-04-16 20:58:12 +0300

    Added week 7 solutions

--- Week7/Problems.hs
+++ Week7/Problems.hs
@@ -14,8 +14,26 @@
 {-# HLINT ignore "Eta reduce" #-}
 
 -- Да се дефинира тип за двоично дърво
+data BTree a = Empty | Node a (BTree a) (BTree a)
+    deriving Show
+
 -- Да се напише функция, която генерира дърво от списък
+generateTree :: [a] -> BTree a 
+generateTree [] = Empty
+generateTree (x : xs) = Node x (generateTree (take (length xs `div` 2) xs)) (generateTree (drop (length xs `div` 2) xs))
+
 -- Да се напише функция, която превръща дърво в списък
+generateList :: BTree a -> [a]
+generateList Empty = [] 
+generateList (Node x left right) = x : generateList left ++ generateList right
+
 -- Да се напише функция, която намира сумата на всички числа в дървото
+sumTree :: BTree Int -> Int 
+sumTree Empty = 0 
+sumTree (Node x l r) = x + sumTree l + sumTree r
+
 -- Да се напише функция, която връща най-дясното листо на дървото
--- Да се напише функция, която добавя нова стойност като най-ляво листо 
+getRightLeaf :: BTree a -> a 
+getRightLeaf Empty = error "oops"
+getRightLeaf (Node x _ Empty) = x
+getRightLeaf (Node _ _ r) = getRightLeaf r
